114246aaac510c4c59e891224c801c15c748c35f
Ari MB <arimb1999@gmail.com>
2020-02-11 19:34:47 +0200

    heatmap works

--- ZebraPlot.js
+++ ZebraPlot.js
@@ -1,6 +1,7 @@
 const events = {
+	'Chezy Champs': '2019cc',
 	'Midwest Regional': '2020ilch',
-	'SBPLI 1': '2020nyli1',
+	'SBPLI 1': '2020nyli',
 	'SBPLI 2': '2020nyli2',
 	'South Florida Regional': '2020flwp',
 	'FIT Greenville': '2020txgre',
@@ -29,11 +30,12 @@ const events = {
 	'IN Perry Meridian': '2020inpmh',
 	'IN District Championship': '2020incmp'
 };
+const tba_api = 'https://www.thebluealliance.com/api/v3';
+const tba_params = 'accept=application/json&X-TBA-Auth-Key=8RP1cDp90o0ODMRwz9uSWYCMINv1qDZacjaZwQJ0NSCaWnyyK2UtS7uc2WGKmzla';
 
 $(document).ready(function(){
 	$('button#menu').click(function(){
-	console.log('hit');
-	$('div#menu').show();
+		$('div#menu').show();
 	});
 	$('span.close').click(function(){
 		$('div#menu').hide();
@@ -44,8 +46,51 @@ $(document).ready(function(){
 	});
 	Object.keys(events).forEach(function(name){$('select#event')[0].append(new Option(name, events[name]))});
 
+	var width = $('img').width();
+	console.log(width);
+	var height = $('img').height();
+	console.log(height);
+	var heatmapInstance = h337.create({container: $('div#canvas-wrapper')[0], radius:1/54*(width*0.914), opacity:0.5, blur:0.6});
 	$('button#go').click(function(){
 		$('div#menu').hide();
-		
+
+		var request = new XMLHttpRequest();
+		request.open('GET', tba_api + '/team/frc' + $('input#team')[0].value + 
+			'/event/' + $('select#event').children('option:selected')[0].value + '/matches/keys?' + tba_params);
+		request.onload = function(){
+			var matches = JSON.parse(this.response);
+			const promises = matches.map(match => new Promise(resolve => 
+				resolve($.getJSON(tba_api + '/match/' + match + '/zebra_motionworks', tba_params))));
+			Promise.all(promises).then(results => {
+				var data = [];
+				results.forEach(match_data => {
+					['blue', 'red'].forEach(alliance => {
+						match_data['alliances'][alliance].forEach(team => {
+							if (team['team_key'] == 'frc'+$('input#team')[0].value){
+								team['xs'].forEach(function(a, i){
+									data.push({
+										x: Math.round((alliance=='red'?a/54:(1-a/54))*(width*0.914)+(width*0.0422)), 
+										y: Math.round((height*0.95)-(alliance=='red'?team['ys'][i]/27:(1-team['ys'][i]/27))*(height*0.901)), 
+										value: 1
+									});
+									// console.log(a);
+									// heatmapInstance.addData({x: (alliance=='blue'?(a*1000/54):(1000/54*(1-a))), y: team['ys'][i]*400/27, value: 1});
+								});
+							}
+						});
+					});
+				});
+				console.log(data);
+				heatmapInstance.setData({
+					max: data.length/500,
+					data: data
+				});
+				heatmapInstance.repaint();
+			});
+		};
+		request.onerror = function(err){console.log(err);};
+		request.send();
 	});
+
+	$('button#go').click();
 });
